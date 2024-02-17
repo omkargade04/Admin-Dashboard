d71e1912ead8945a1e630a04e5f20b0fd835f2f1
Omkar Gade <g.omkar1110@gmail.com>
2024-02-18 02:44:57 +0530

    cors issue fixed

--- server/server.js
+++ server/server.js
@@ -17,10 +17,13 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Credentials', true);
+app.use((_req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', '*');
+
   next();
 });
+
 
 // const corsOptions = {
 //   origin: '*',
