35c1e7417e492f325ec4c9a8d1d72bca7b3accdd
Omkar Gade <g.omkar1110@gmail.com>
2024-02-18 01:48:40 +0530

    pkg json updated

--- server/server.js
+++ server/server.js
@@ -9,17 +9,17 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 
-const corsOptions = {
-  origin: `${process.env.NEXT_URL}`,
-  credentials: true,
-};
+// const corsOptions = {
+//   origin: `${process.env.NEXT_URL}`,
+//   credentials: true,
+// };
 
 app.use(cors());
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Credentials', true);
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Credentials', true);
+//   next();
+// });
 
 app.use(express.urlencoded({ extended: true }));
 
