41a79d5d4ec92b7f76061712460ddd6c4be38da4
Omkar Gade <g.omkar1110@gmail.com>
2024-02-18 20:25:40 +0530

    blank cors

--- server/server.js
+++ server/server.js
@@ -9,19 +9,7 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 
-const whitelist = ["https://admin-dashboard-xi-woad.vercel.app"];
-
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use(express.urlencoded({ extended: true }));
 
