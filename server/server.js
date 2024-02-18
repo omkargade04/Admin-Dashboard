a1f171d724bcf3ee8e83726ab3dc8c169b813f33
Omkar Gade <g.omkar1110@gmail.com>
2024-02-18 20:33:30 +0530

    cors updated

--- server/server.js
+++ server/server.js
@@ -9,7 +9,13 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 
-app.use(cors());
+const corsOptions = {
+  origin: ["*"],
+  methods: ["GET", "PUT", "POST", "DELETE"],
+  credentials: false,
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.urlencoded({ extended: true }));
 
