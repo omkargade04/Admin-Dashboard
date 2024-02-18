eeac41536634cf0b03dd699861107cb745e185fa
Omkar Gade <g.omkar1110@gmail.com>
2024-02-18 19:53:47 +0530

    cors deleted

--- server/server.js
+++ server/server.js
@@ -9,28 +9,7 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 
-const corsOptions = {
-  origin: ['http://localhost:3000'],
-  methods: ["GET","PUT","POST","DELETE"],
-  credentials: true,
-};
-
-app.use(cors(corsOptions));
-
-// app.use((_req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header('Access-Control-Allow-Headers', '*');
-//   next();
-// });
-
-
-// const corsOptions = {
-//   origin: '*',
-//   credentials: true,
-// };
-
-// app.use(cors(corsOptions));
-
+app.use(cors());
 
 app.use(express.urlencoded({ extended: true }));
 
